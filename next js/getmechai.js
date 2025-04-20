dd690b230865e6d56a2d02c8d70bfc5230bd5a8d
Ranajay Saha <111520445+jaysaha01@users.noreply.github.com>
2025-04-20 07:21:04 +0530

    Delete next js/getmechai.js

--- next js/getmechai.js
+++
@@ -1,7 +0,0 @@
-// Sign Up 
-
-//Go to github > setting > Developer Setting > Auth Apps >  new Auth Apps > next-auth-redux-toolkit, locahost:3000/api/auth/callback/github (get from auth.js configation Github for OAuth) > Register Application
-
-
-
-
